40ce3445ca94850b652e72eae55caf0d6b193e8e
brasi86 <lenny.marchio@gmail.com>
2024-03-11 13:42:57 +0100

    creazione pagine e routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+
 export default function App() {
-  return <h1 className="">APP</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignOut />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
