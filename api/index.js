14ccb90ec358e68c4333dc6b5e251fd3b886b02a
brasi86 <lenny.marchio@gmail.com>
2024-03-11 17:45:38 +0100

    creazione signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoute from "./routes/user.route.js";
+import authRoute from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -15,8 +16,10 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
 
 app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(3000, () => {
   console.log("Server run on port 3000");
